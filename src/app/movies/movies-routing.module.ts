9a3b8ef7fb2e6957fb982ea537d282b366a6255c
unknown <giorgos.mikelis@gmail.com>
2020-10-22 16:53:02 +0300

    routing first com

--- src/app/movies/movies-routing.module.ts
+++ src/app/movies/movies-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllMoviesComponent } from './all-movies/all-movies.component';
+import { FavoriteMoviesComponent } from './favorite-movies/favorite-movies.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MoviesComponent,
+    children: [
+      {
+        path: '', component: HomeComponent
+      },
+      {
+        path: 'all-movies', component: AllMoviesComponent
+      },
+      {
+        path: 'favorite-movies', component: FavoriteMoviesComponent
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
