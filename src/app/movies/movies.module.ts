44895d0d3fd684f5325a3e0e1dd1c1af35eba1a4
unknown <giorgos.mikelis@gmail.com>
2020-10-22 13:58:03 +0300

    skeleton 2

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -6,10 +6,12 @@ import { MoviesComponent } from './movies.component';
 import { HomeComponent } from './home/home.component';
 import { AllMoviesComponent } from './all-movies/all-movies.component';
 import { FavoriteMoviesComponent } from './favorite-movies/favorite-movies.component';
+import { MovieItemComponent } from './movie-item/movie-item.component';
+import { MoviesSearchComponent } from './movies-search/movies-search.component';
 
 
 @NgModule({
-  declarations: [MoviesComponent, HomeComponent, AllMoviesComponent, FavoriteMoviesComponent],
+  declarations: [MoviesComponent, HomeComponent, AllMoviesComponent, FavoriteMoviesComponent, MovieItemComponent, MoviesSearchComponent],
   imports: [
     CommonModule,
     MoviesRoutingModule
